3715980bacb276fad48cf48d618e5e447a51e69f
Digdgeo <digd.geografo@gmail.com>
2024-08-18 13:24:00 +0200

    Debugging Products

--- code/productos.py
+++ code/productos.py
@@ -4,6 +4,13 @@ import matplotlib.pyplot as plt
 from osgeo import gdal, gdalconst
 from datetime import datetime, date
 
+
+from pymongo import MongoClient
+client = MongoClient()
+
+database = client.Satelites
+db = database.Landsat
+
 class Product(object):
     
     
@@ -12,85 +19,56 @@ class Product(object):
         
     def __init__(self, ruta_nor):
         
-        self.ruta_escena = ruta_nor
-        self.escena = os.path.split(self.ruta_escena)[1]
-        self.raiz = os.path.split(os.path.split(self.ruta_escena)[0])[0]
-        print(self.raiz)
-        self.nor = os.path.join(self.raiz, os.path.join('nor', self.escena))
-        print(self.nor)
-        self.ori = os.path.join(self.raiz, os.path.join('ori', self.escena))
+        
+        self.escena = os.path.split(ruta_nor)[1]
+        self.raiz = os.path.split(ruta_nor)[0]
+        self.ori = os.path.join(self.raiz, 'ori')
+        self.nor = os.path.join(self.raiz, 'nor')
+        self.productos = os.path.join(self.raiz, 'pro')
         self.data = os.path.join(self.raiz, 'data')
         self.temp = os.path.join(self.raiz, 'temp')
-        self.productos = os.path.join(self.raiz, 'pro')
-        self.pro_esc = os.path.join(self.productos, self.escena)
-        os.makedirs(self.pro_esc, exist_ok=True)
+        os.makedirs(self.temp, exist_ok=True)
         
-        if 'l8oli' in self.ruta_escena:
-            self.sat = 'L8'
-        elif 'l7etm' in self.escena:
-            self.sat = 'L7'
-        elif 'l5tm' in self.ruta_escena:
-            self.sat =  'L5'
-        elif 'l4tm' in self.ruta_escena:
-            self.sat =  'L5'
-        else:
-            print('No identifico el satelite')
-        
-        print(self.sat)
-        
-        if self.sat == 'L8':
+        self.ori_escena = os.path.join(self.raiz, os.path.join('ori', self.escena))
+        self.nor_escena = ruta_nor
+        self.pro_escena = os.path.join(self.productos, self.escena)
+        os.makedirs(self.pro_escena, exist_ok=True)
 
-            for i in os.listdir(self.nor):
-                if re.search('tif$', i):
-                    
-                    banda = i[-6:-4].lower()
-                                        
-                    if banda == 'b2':
-                        self.blue = os.path.join(self.nor, i)
-                    elif banda == 'b3':
-                        self.green = os.path.join(self.nor, i)
-                    elif banda == 'b4':
-                        self.red = os.path.join(self.nor, i)
-                    elif banda == 'b5':
-                        self.nir = os.path.join(self.nor, i)
-                    elif banda == 'b6':
-                        self.swir1 = os.path.join(self.nor, i)
-                    elif banda == 'b7':
-                        self.swir2 = os.path.join(self.nor, i)
-                    elif banda == 'k4':
-                        self.fmask = os.path.join(self.nor, i)
-                    
-        else:
+        for i in os.listdir(self.nor_escena):
+            if re.search('tif$', i):
+                # Verificamos si el archivo es 'fmask' o 'hillshade'
+                if 'fmask' in i:
+                    self.fmask = os.path.join(self.nor, i)
+                elif 'hillshade' in i:
+                    self.hillshade = os.path.join(self.nor, i)
+                else:
+                    # Dividimos el nombre del archivo en partes
+                    partes = i.split('_')
+                    # Comprobamos si tiene al menos 3 partes (para evitar el error de índice)
+                    if len(partes) >= 3:
+                        banda = partes[-2]
+                        if banda == 'blue':
+                            self.blue = os.path.join(self.nor, i)
+                        elif banda == 'green':
+                            self.green = os.path.join(self.nor, i)
+                        elif banda == 'red':
+                            self.red = os.path.join(self.nor, i)
+                        elif banda == 'nir':
+                            self.nir = os.path.join(self.nor, i)
+                        elif banda == 'swir1':
+                            self.swir1 = os.path.join(self.nor, i)
+                        elif banda == 'swir2':
+                            self.swir2 = os.path.join(self.nor, i)
 
-            for i in os.listdir(self.nor):
-                if re.search('tif$', i):
-                    
-                    banda = i[-6:-4].lower()
-                                        
-                    if banda == 'b1':
-                        self.blue = os.path.join(self.nor, i)
-                    elif banda == 'b2':
-                        self.green = os.path.join(self.nor, i)
-                    elif banda == 'b3':
-                        self.red = os.path.join(self.nor, i)
-                    elif banda == 'b4':
-                        self.nir = os.path.join(self.nor, i)
-                    elif banda == 'b5':
-                        self.swir1 = os.path.join(self.nor, i)
-                    elif banda == 'b7':
-                        self.swir2 = os.path.join(self.nor, i)
-                    elif banda == 'k4':
-                        self.fmask = os.path.join(self.nor, i)
-        
-        #Insertamos la cobertura de nubes en la BD
-        connection = pymongo.MongoClient("mongodb://localhost")
-        db=connection.teledeteccion
-        landsat = db.landsat
-        
-        
+        # Debugging print statements
+        print('BLUE:', self.blue)
+        print('FMASK:', self.fmask)
+        print('HILLSHADE:', self.hillshade)
+
+       
         try:
         
-            landsat.update_one({'_id':self.escena}, {'$set':{'Productos': []}},  upsert=True)
+            db.update_one({'_id':self.escena}, {'$set':{'Productos': []}},  upsert=True)
             
         except Exception as e:
             print("Unexpected error:", type(e), e)
